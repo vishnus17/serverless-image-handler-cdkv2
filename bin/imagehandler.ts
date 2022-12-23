16aba1a64dca6e972654550cc372c2ad7a5b4153
Vishnu S <58102524+vishnus17@users.noreply.github.com>
2022-12-24 01:14:02 +0530

    added article link to readme

--- bin/imagehandler.ts
+++ bin/imagehandler.ts
@@ -2,12 +2,13 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ImagehandlerStack } from '../lib/imagehandler-stack';
+import { env } from 'process';
 
 const app = new cdk.App();
 
 new ImagehandlerStack(app, 'ImagehandlerStack', {
     stageName: 'sbx',
-    bucketname: 'enter-bucket-name',
+    bucketname: 'source-image-bucket-1',
     appName: 'sih',
-    env: { account: 'account_number', region: 'region_name' }
+    env: { account: env.account, region: env.region }
 });
